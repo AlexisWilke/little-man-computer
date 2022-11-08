8aec9899c30705e2f060ec57d055fb96976ec947
AlexisWilke <alexis@m2osw.com>
2022-11-07 21:27:52 -0800

    Added a README and a LICENSE.

---
+++ README.md
@@ -0,0 +1,15 @@
+
+# Compile
+
+On Ubuntu, you shoudl have no problem with:
+
+    ./build
+
+and then run with:
+
+    BUILD/little-man-computer <file>.lmc
+
+To see the _binary_, use the `-s` option:
+
+    BUILD/little-man-computer -s <file>.lmc
+
--- little-man-computer.cpp
+++ little-man-computer.cpp
@@ -1,3 +1,22 @@
+// Written by Alexis Wilke based on
+// https://en.wikipedia.org/wiki/Little_man_computer
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <https://www.gnu.org/licenses/>.
+//
+// Source: 
+// https://github.com/AlexisWilke/little-man-computer
+
 
 #include    <cstring>
 #include    <fstream>
